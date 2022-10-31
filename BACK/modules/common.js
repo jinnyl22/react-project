7b886c7343c9d490b4690670d99bc5965d08771f
jinnyl22 <smile2374@gmail.com>
2022-10-31 17:06:50 +0900

    back도 새로운 마음가짐으로 새출발

--- BACK/modules/common.js
+++
@@ -1,7 +0,0 @@
-// 모듈을 모아놓고 관리하는 곳
-const express = require("express");
-const session = require("express-session");
-const dot = require("dotenv");
-const mailer = require("nodemailer");
-
-module.exports = { express, session, dot, mailer };
--- BACK/service/index.js
+++
@@ -1,4 +0,0 @@
-// const userService = require("./user_service");
-
-// module.exports = { userService };
-// 나중에 컨트롤러랑 분리하면 사용하고 분리 안하면 굳이...?
